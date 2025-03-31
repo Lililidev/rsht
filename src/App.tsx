62dcfb95136966aa5b9ddbd742d8d89b014fb121
Lililidev <elizaveta.vadimovna@yandex.ru>
2025-03-31 23:30:38 +0300

    app and routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
-import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import FileList from '@/components/FileList/FileList';
 
-const App: React.FC = () => {
-  return <div>File system</div>;
-};
+const theme = createTheme();
 
-export default App;
+export default function App() {
+  return (
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<FileList />} />
+          <Route path="/folder/:folderId" element={<FileList />} />
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
+  );
+}
